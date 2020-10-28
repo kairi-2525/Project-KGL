c67f1db80c66364869e0627749a49b71b65c07ff
kairi <kairisksv@gmail.com>
2020-10-28 12:38:47 +0900

    パーティクルのMipMapのテスト

--- Project/ExecutionProject/Game/HLSL/3D/Particle_ps.hlsl
+++ Project/ExecutionProject/Game/HLSL/3D/Particle_ps.hlsl
@@ -12,7 +12,7 @@ struct PSOut
 PSOut PSMain(PSInput input) : SV_TARGET
 {
 	PSOut output = (PSOut)0;
-	const float4 color = tex[input.tex_id - (input.tex_id * int(zero_texture))].SampleLevel(smp, input.uv, 0) * input.color;
+	const float4 color = tex[input.tex_id - (input.tex_id * int(zero_texture))].SampleLevel(smp, input.uv, (clamp(input.pos.z, 0.f, 0.01f) * 100.f * (5.f - 1.f))) * input.color;
 	if (input.bloom)
 	{
 		output.bloom_color = color;
